c645a872636ed37abf9b31f26eca6536d302b6f1
ashes <ashkaneslamii1993@gmail.com>
2026-05-29 09:49:45 +0330

    feat: removed the name of the mood from filter bar for more compact feature

--- src/components/home/FilterBar.tsx
+++ src/components/home/FilterBar.tsx
@@ -47,7 +47,6 @@ export default function FilterBar({
               }`}
             >
               <span className="mr-1">{mood.emoji}</span>
-              {mood.label}
             </button>
           );
         })}
