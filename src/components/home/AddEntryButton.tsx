359c37739a12ccd1f4b93f121af4877f0f95950a
ashes <ashkaneslamii1993@gmail.com>
2026-05-29 09:35:43 +0330

    feat: Optimize the button UI

--- src/components/home/AddEntryButton.tsx
+++ src/components/home/AddEntryButton.tsx
@@ -3,10 +3,10 @@
 export default function AddEntryButton({ onClick }: { onClick: () => void }) {
   return (
     <button
-      className="bg-blue-500 text-white absolute bottom-8 right-8 px-4 py-2 rounded hover:bg-blue-600"
+      className="bg-blue-500 text-white text-3xl absolute bottom-8 right-8 px-4 py-2 rounded-full hover:bg-blue-600 hover:shadow-lg hover:scale-105 transition-all duration-300"
       onClick={onClick}
     >
-      Add Entry
+      +
     </button>
   );
 }
