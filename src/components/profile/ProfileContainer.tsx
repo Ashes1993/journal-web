6eceee1e6f1e3e54b941595333af9b1df16ee231
Ashkan <ashkan.eddyking@gmail.com>
2026-06-10 19:24:16 +0400

    style: add padding to the bottom for better mobile view design

--- src/components/profile/ProfileContainer.tsx
+++ src/components/profile/ProfileContainer.tsx
@@ -14,7 +14,7 @@ interface ProfileContainerProps {
 
 export default function ProfileContainer({ user }: ProfileContainerProps) {
   return (
-    <div className="mx-auto max-w-4xl space-y-8 p-4 sm:p-6 lg:p-8">
+    <div className="mx-auto max-w-4xl space-y-8 pt-4 px-4 pb-20 md:p-6 lg:p-8">
       {/* 1. Identity Spotlight Block */}
       <ProfileHeader user={user} />
 
